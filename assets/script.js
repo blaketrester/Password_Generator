5d3cad80e6412fd31ed769c7a5998064447fe756
Blake Trester <blaketrester@yahoo.com>
2022-08-13 23:34:32 -0600

    Added 'math' for statements to finalize functionality

--- assets/script.js
+++ assets/script.js
@@ -4,14 +4,17 @@ var generateBtn = document.querySelector("#generate"); // Don't Touch!
 var characterLength = 8;
 var choiceArray = [];
 
+// Possible characters for output
 var specialCharArray = ["!", "@", "#", "$","%", "&", "*", "?"];
 var lowerCaseArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var upperCaseArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var numberArray = ["1","2","3","4","5","6","7","8","9","0"];
 
-function generatePassword() {
-
-  var userChoice = window.prompt("Please choose a password length between 8 and 128");
+// Prompts for user
+function getPrompts() {
+  choiceArray =[];
+ 
+  userChoice = window.prompt("Please choose a password length between 8 and 128");
 
   if (userChoice < 8 || userChoice > 128) {
     alert ("Must be between 8 and 128")
@@ -34,16 +37,33 @@ function generatePassword() {
     choiceArray = choiceArray.concat(numberArray);
   }
 
-  return "Generated password will go here"
+  return true;
 }
 
-// Write password to the #password input
+// Generate password based on user preferance
+function generatePassword(){
+
+  var password = "";
+  for(var i = 0; i < userChoice; i++) {
+    var random = Math.floor(Math.random() * choiceArray.length);
+    password = password + choiceArray[random];
+  }
+  return password;
+}
+
+// Write password to the #password inputs
 function writePassword() {
+
+  var truePrompt = getPrompts();
+
+  if (truePrompt) {
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
+}
 }
 
 // Add event listener to generate button
