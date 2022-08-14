8ef8f23dacfcad60c2b4620f44532fcd1d0dc220
Blake Trester <blaketrester@yahoo.com>
2022-08-13 21:58:03 -0600

    Added all of the possible characters that can be added to the generated password

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,30 @@
 // Assignment code here
+var generateBtn = document.querySelector("#generate"); // Don't Touch!
 
+var characterLength = 8;
+var choiceArray = [];
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+var specialCharArray = ["!", "@", "#", "$","%", "&", "*", "?"];
+var lowerCaseArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperCaseArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var numberArray = ["1","2","3","4","5","6","7","8","9","0"];
+
+function generatePassword() {
+
+  var userChoice = window.prompt("Please choose a password length between 8 and 128");
+
+  if (userChoice < 8) {
+    alert ("Must be between 8 and 128")
+    return;
+  }
+
+  if (userChoice > 128) {
+    alert ("Must be between 8 and 128")
+    return;
+  }
+
+  return "Generated password will go here"
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -14,4 +36,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); // Don't Touch!
